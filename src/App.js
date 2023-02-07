05f1c33fa5ff55b28b81294c2cfb0e1f6ab3b2d0
fahreza raharjo <fahrezaraharjo92@gmail.com>
2023-02-07 15:24:57 +0700

    setup project

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { createBrowserRouter, Outlet, RouterProvider, } from "react-router-dom";
+import { Home, Login, Register, Single, Write } from "./pages";
+import { Navbar, Footer } from "./components";
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/post/:id',
+        element: <Single />
+      },
+      {
+        path: '/write',
+        element: <Write />
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+      <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export { default as Home } from './Home'
+export { default as Register } from './Register'
+export { default as Login } from './Login'
+export { default as Write } from './Write'
+export { default as Single } from './Single'
+
+
+
