1c36258ce58e9e7860956993ef1fa333bd7811b3
fahreza raharjo <fahrezaraharjo92@gmail.com>
2023-02-08 15:00:08 +0700

    done home footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="post">
+        {/* {posts.map(post => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt="" />
+            </div>
+            <div className="content">
+              <Link className='link' to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+                <p>{post.desc}</p>
+                <button>Read more...</button>
+            </div>
+          </div>
+        ))} */}
+      </div>
+    </div>
   )
 }
 
